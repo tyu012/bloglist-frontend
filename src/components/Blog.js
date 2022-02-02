575b646c46c725982506925b49c7f86cff223102
Tim Yu <yuyu.tim@gmail.com>
2022-02-01 21:22:20 -0800

    implemented display of comments from backend

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import PropTypes from 'prop-types'
 
 const Blog = ({ blog, user, likeBlog, removeBlog }) => {
   if (!blog) {
@@ -42,77 +41,24 @@ const Blog = ({ blog, user, likeBlog, removeBlog }) => {
       <div style={showToPoster}>
         <button onClick={handleRemove}>remove</button>
       </div>
+      <div>
+        <Comments comments={blog.comments} />
+      </div>
     </div>
   )
 }
 
-// export const LegacyBlog = ({ blog, user, likeBlog, removeBlog }) => {
-//   console.log(blog)
-//   const [detailed, setDetailed] = useState(false)
-
-//   const toggleDetail = event => {
-//     event.preventDefault()
-//     setDetailed(!detailed)
-//   }
-
-//   const handleLike = async event => {
-//     event.preventDefault()
-//     await likeBlog(blog)
-//   }
-
-//   const handleRemove = async event => {
-//     event.preventDefault()
-//     if (window.confirm(`Remove blog ${blog.title} by ${blog.author}`)) {
-//       await removeBlog(blog)
-//     }
-//   }
-
-//   const isPoster = () => user.username === blog.user.username
-
-//   const showWhenDetailed = { display: detailed ? '' : 'none' }
-//   const showToPosterWhenDetailed = { display: detailed && isPoster() ? '' : 'none' }
-
-//   const blogStyle = {
-//     paddingTop: 10,
-//     paddingLeft: 2,
-//     border: 'solid',
-//     borderWidth: 1,
-//     marginBottom: 5
-//   }
-
-//   return (
-//     <div style={blogStyle} className="blog">
-//       <div className="basicBlogInfo">
-//         {blog.title} {blog.author}
-//         <button onClick={toggleDetail}>{detailed ? 'hide' : 'view'}</button>
-//       </div>
-
-//       <div style={showWhenDetailed} className="detailedBlogInfo">
-//         <div>
-//           {blog.url}
-//         </div>
-
-//         <div className="blogLikes">
-//           likes <span>{blog.likes}</span>
-//           <button onClick={handleLike}>like</button>
-//         </div>
-
-//         <div>
-//           {blog.user.name}
-//         </div>
-
-//         <div style={showToPosterWhenDetailed}>
-//           <button onClick={handleRemove}>remove</button>
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-
-// LegacyBlog.propTypes = {
-//   blog: PropTypes.object.isRequired,
-//   likeBlog: PropTypes.func.isRequired,
-//   removeBlog: PropTypes.func.isRequired,
-// }
+const Comments = ({ comments }) => {
+  return (
+    <div>
+      <h4>comments</h4>
+      <ul>
+        {comments.map((c, i) =>
+          <li key={i}>{c}</li>
+        )}
+      </ul>
+    </div>
+  )
+}
 
 export default Blog
